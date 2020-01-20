5243e6dba6c247ff8d736ccab5c6e8771a67c7cd
Antonio Gabriel <antoniogdelrio@hotmail.com>
2020-01-20 16:55:12 -0300

    deleting joker buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,11 @@
 import React from 'react';
 import './App.css';
 import Router from './routes'
-import LeftJokerButton from '../src/components/LeftJokerButton/LeftJokerButton'
-import RightJokerButton from '../src/components/RightJokerButton/RightJokerButton'
 
 function App() {
-  const defineMenu = () => {
-    if(window.location.pathname === '/' || window.location.pathname === '/dashboard/aluno'){
-      return("menu-reverse");
-    }
-    else{
-      return("menu");
-    }
-  }
 
   return (
     <>
-      <div className={defineMenu()}>
-        <LeftJokerButton/>
-        <RightJokerButton/>
-      </div>  
       <div className="container">
         <Router />
       </div>
